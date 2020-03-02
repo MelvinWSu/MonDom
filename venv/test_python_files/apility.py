2304297f99e49b7ee40ac7c4ae5c6175b4c72396
jt7898 <32527413+jt7898@users.noreply.github.com>
2020-03-01 17:57:39 -0800

    apility testing

---
+++ venv/test_python_files/apility.py
@@ -0,0 +1,18 @@
+import apilityio
+import requests
+import validators
+
+client = apilityio.Client(api_key='028d21b2-bad9-4db6-8564-0f60159ee65b')
+
+#TEST
+ip = '0.21.6.27'
+response = client.CheckIP(ip)
+
+if response.status_code == 404:
+    print("Congratulations! The IP address has not been found in any blacklist.")
+
+if response.status_code == 200:
+    print("Ooops! The IP address has been found in one or more blacklist")
+    blacklists = response.blacklists
+    print('+- Blacklists: %s' % blacklists)
+
