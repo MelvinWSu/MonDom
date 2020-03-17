2921215a238f3c440342141716671d4ece02c8cc
jt7898 <32527413+jt7898@users.noreply.github.com>
2020-03-17 13:22:36 -0700

    Update list_of_ip_test.py

--- venv/test_python_files/list_of_ip_test.py
+++ venv/test_python_files/list_of_ip_test.py
@@ -8,11 +8,26 @@ ip_list = export.values.T[0].tolist()
 
 client = apilityio.Client(api_key='028d21b2-bad9-4db6-8564-0f60159ee65b')
 
-for item in ip_list:
-    response = client.CheckIP(item)
 
-    if response.status_code == 404:
-        print("Good IP")
+# This is to get IP and their corresponding blacklists (if any) using CheckBatchIP()
+# It is faster than iterating through each ip in the list and getting their individual HTTP responses
+response = client.CheckBatchIP(ip_list)
+for item in response.json.get("response"):
+    print(item.get("ip"))
+    if not item.get("blacklists"):
+        print("Not Found")
+    else:
+        print(item.get("blacklists"))
 
-    if response.status_code == 200:
-        print("Bad IP")
+# This gets an HTTP response back from each individual ip using CheckIP()
+# It is slower than getting them all from a batch request using CheckBatchIP()
+#print(response.json.get("response"))
+#
+# for item in ip_list:
+#     response = client.CheckIP(item)
+#
+#     if response.status_code == 404:
+#         print("Good IP")
+#
+#     if response.status_code == 200:
+#         print("Bad IP")
